254da353fa03af3a6d83c406779621307c255a91
HKMUSIC <himanshu4hk9@gmail.com>
2025-08-09 15:21:24 +0530

    Update git.py

--- SHUKLAMUSIC/core/git.py
+++ SHUKLAMUSIC/core/git.py
@@ -6,7 +6,6 @@ from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError
 
 import config
-
 from ..logging import LOGGER
 
 
@@ -37,28 +36,18 @@ def git():
         UPSTREAM_REPO = f"https://{GIT_USERNAME}:{config.GIT_TOKEN}@{TEMP_REPO}"
     else:
         UPSTREAM_REPO = config.UPSTREAM_REPO
+
     try:
         repo = Repo()
-        LOGGER(__name__).info(f"Git Client Found [VPS DEPLOYER]")
+        LOGGER(__name__).info("Git Client Found [VPS DEPLOYER]")
     except GitCommandError:
-        LOGGER(__name__).info(f"Invalid Git Command")
+        LOGGER(__name__).info("Invalid Git Command")
+        return
     except InvalidGitRepositoryError:
-        def git():
-    REPO_LINK = config.UPSTREAM_REPO
-    if config.GIT_TOKEN:
-        GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
-        TEMP_REPO = REPO_LINK.split("https://")[1]
-        UPSTREAM_REPO = f"https://{GIT_USERNAME}:{config.GIT_TOKEN}@{TEMP_REPO}"
-    else:
-        UPSTREAM_REPO = config.UPSTREAM_REPO
-    try:
-        repo = Repo()
-        LOGGER(__name__).info(f"Git Client Found [VPS DEPLOYER]")
-    except InvalidGitRepositoryError:
-        LOGGER(__name__).info(f"Skipping git fetch on Heroku to prevent crash")
+        LOGGER(__name__).info("Skipping git fetch on Heroku to prevent crash")
         return
 
-    # Heroku pe fetch disable
+    # Heroku pe fetch disable ka code comment me rakha
     # repo = Repo.init()
     # if "origin" in repo.remotes:
     #     origin = repo.remote("origin")
@@ -73,15 +62,13 @@ def git():
     #     origin.refs[config.UPSTREAM_BRANCH]
     # )
     # repo.heads[config.UPSTREAM_BRANCH].checkout(True)
-    # try:
-    #     repo.create_remote("origin", config.UPSTREAM_REPO)
-    # except BaseException:
-    #     pass
-        nrs = repo.remote("origin")
-        nrs.fetch(config.UPSTREAM_BRANCH)
-        try:
-            nrs.pull(config.UPSTREAM_BRANCH)
-        except GitCommandError:
-            repo.git.reset("--hard", "FETCH_HEAD")
-        install_req("pip3 install --no-cache-dir -r requirements.txt")
-        LOGGER(__name__).info(f"Fetching updates from upstream repository...")
+
+    nrs = repo.remote("origin")
+    nrs.fetch(config.UPSTREAM_BRANCH)
+    try:
+        nrs.pull(config.UPSTREAM_BRANCH)
+    except GitCommandError:
+        repo.git.reset("--hard", "FETCH_HEAD")
+
+    install_req("pip3 install --no-cache-dir -r requirements.txt")
+    LOGGER(__name__).info("Fetching updates from upstream repository...")
